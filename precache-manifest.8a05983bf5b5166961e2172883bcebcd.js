040c768b68770ac03a565115cc8e95abd9508d29
elsiddig <parsercoder@gmail.com>
2020-09-19 13:08:08 +0200

    Updates

---
+++ precache-manifest.8a05983bf5b5166961e2172883bcebcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef32d2573a9f25f759ae535191c73f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "676178c62723e7a90863",
+    "url": "./static/css/main.40ea4bdf.chunk.css"
+  },
+  {
+    "revision": "cbd85e94b0520c78546c",
+    "url": "./static/js/2.fd464318.chunk.js"
+  },
+  {
+    "revision": "64b96940649087d29022e940dbb97b7c",
+    "url": "./static/js/2.fd464318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676178c62723e7a90863",
+    "url": "./static/js/main.9f712ce1.chunk.js"
+  },
+  {
+    "revision": "6abe41bcdd1a5d203472",
+    "url": "./static/js/runtime-main.4966948f.js"
+  }
+]);
